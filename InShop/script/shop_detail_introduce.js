58d84913395aca6f6c3b0732b478178881c5f800
fengchaoqun <1525205286@qq.com>
2016-11-07 22:22:40 +0800

    增加放大镜效果
    为商品详细页添加图片放大镜效果以及完善列表小图片的切换功能

--- InShop/script/shop_detail_introduce.js
+++ InShop/script/shop_detail_introduce.js
@@ -1,46 +1,54 @@
 // JavaScript Document
 window.onload = function(){
 	var des_image = document.getElementById("description_image");
-	var bigBox = document.getElementById("big_image");
+	var midBox = document.getElementById("mid_img_box");
 	var mark = document.getElementById("mark");
 	var floatBox = document.getElementById("float-box");
-	var magBigBox = document.getElementById("mag_big_image");
-	var magBoxImg = magBigBox.getElementsByTagName("img");
+	var bigImgBox = document.getElementById("big_img_box");
+	var bigImg = bigImgBox.getElementsByTagName("img")[0];
 	
 	mark.onmouseover = function(){
 		floatBox.style.display = "block";
-		magBigBox.style.display = "block";
+		bigImgBox.style.display = "block";
 	}
+	
 	mark.onmouseout = function(){
 		floatBox.style.display = "none";
-		magBigBox.style.display = "none";
+		bigImgBox.style.display = "none";
 	}
-	mark.onmousemove = function(e){
-		var ev = e || window.event;
+	
+	mark.onmousemove = function(ev){
+		var e = ev || window.event; //兼容IE
+		var left = e.clientX - des_image.offsetLeft - midBox.offsetLeft - floatBox.offsetWidth / 2;
+		//此处会有一个浏览器滚动条滚动的问题
+		//但是要考虑兼容性问题
+		//可以使用body.scrollTop 所有都支持
+		var top =document.body.scrollTop + e.clientY - des_image.offsetTop - midBox.offsetTop - floatBox.offsetHeight / 2;
 		
-		var left = ev.clientX -  des_image.offsetLeft - bigBox.offsetLeft - floatBox.offsetWidth / 2;
-		var top = ev.clientY - des_image.offsetTop - bigBox.offsetTop - floatBox.offsetHeight/2;
-		
-		if(left<0){
+		//当到midBox最左边的时候和最右边的时候
+		if( left<0 ){
 			left = 0;
-		}else if(left>(mark.offsetWidth - floatBox.offsetWidth)){
+		}else if(left > (mark.offsetWidth - floatBox.offsetWidth)){
 			left = mark.offsetWidth - floatBox.offsetWidth;
 		}
 		
-		if(top<0){
+		//当到midBox最上边的时候和最下边的时候
+		if( top<0 ){
 			top = 0;
-		}else if(top>(mark.offsetHeight-floatBox.offsetHeight)){
-			top = mark.offsetHeight - floatBox.offsetHeight; 
+		}else if( top>mark.offsetHeight - floatBox.offsetHeight ){
+			top = mark.offsetHeight - floatBox.offsetHeight;
 		}
-		
+		//设置放大镜的位置
 		floatBox.style.left = left + "px";
 		floatBox.style.top = top +"px";
 		
+		//百分比
 		var percentX = left / (mark.offsetWidth - floatBox.offsetWidth);
-		var percentY = top/(mark.offsetHeight - floatBox.offsetHeight);
-		for(var i=0;i<magBoxImg.length;i++){
-			magBoxImg[i].style.left = -percentX * (magBoxImg[i].offsetWidth - magBigBox.offsetWidth) + "px";
-			magBoxImg[i].style.top = -percentY * (magBoxImg[i].offsetHeight - magBigBox.offsetHeight)+"px";
-		}
+		var percentY = top / (mark.offsetHeight - floatBox.offsetHeight);
+		
+		//设置大图片宽高
+		bigImg.style.left = - percentX * (bigImg.offsetWidth - bigImgBox.offsetWidth) + "px";
+		bigImg.style.top = - percentY * (bigImg.offsetHeight - bigImgBox.offsetHeight) + "px";
+		
 	}
 }
--- InShop/script/shop_detail_introduce_support.js
+++ InShop/script/shop_detail_introduce_support.js
@@ -34,3 +34,91 @@
 		}
 	}
 	
+	//商品图片浏览区切换图片
+	function changeImg_Left(){
+		var bigImg = document.getElementById("big_img");
+		var midImg = document.getElementById("mid_image");
+		var smallImgBox = document.getElementById("small_img_box");
+		var lis = smallImgBox.getElementsByTagName("li");
+		var index = 0;
+		for(var i=0;i<lis.length;i++){
+			var img = lis[i].getElementsByTagName("img")[0];
+			if(img.className=="img_active"){
+				index = i;
+				index--;
+				if(index<0){
+					return;
+				}else if(index==0){
+					midImg.src = "img/products/shop_detail_item1/item1.jpg";
+					bigImg.src="img/products/shop_detail_item1/item1_big.jpg";
+					
+				}else if(index==1){
+					
+					midImg.src = "img/products/shop_detail_item1/item2.jpg";
+					bigImg.src="img/products/shop_detail_item1/item2_big.jpg";
+				}
+				img.className="";
+				lis[index].getElementsByTagName("img")[0].className ="img_active";
+				break;
+			}
+		} 
+	}	
+	//商品图片浏览区切换图片
+	function changeImg_Right(){
+		var bigImg = document.getElementById("big_img");
+		var midImg = document.getElementById("mid_image");
+		var smallImgBox = document.getElementById("small_img_box");
+		var lis = smallImgBox.getElementsByTagName("li");
+		var index = 0;
+		for(var i=0;i<lis.length;i++){
+			var img = lis[i].getElementsByTagName("img")[0];
+			if(img.className=="img_active"){
+				index = i;
+				index++;
+				//判读是否大于2是因为一共三张图
+				if(index>2){
+					return;
+				}else if(index==1){
+					//更改资源文件
+					midImg.src = "img/products/shop_detail_item1/item2.jpg";
+					bigImg.src="img/products/shop_detail_item1/item2_big.jpg";
+					
+				}else if(index==2){
+					midImg.src = "img/products/shop_detail_item1/item3.jpg";
+					bigImg.src="img/products/shop_detail_item1/item3_big.jpg";
+				}
+				//改变底部小图片的选中样式
+				img.className="";
+				lis[index].getElementsByTagName("img")[0].className ="img_active";
+				break;
+			}
+		} 
+		
+		
+	}
+	
+	
+	
+	
+	//增加购买数量
+	function numInc(){
+		var maxAmount = parseInt(document.getElementById("maxAmount").innerHTML);
+		var amount = parseInt(document.getElementById("amount").value);
+		if(amount >= maxAmount){
+			alert("已经达到购买数量的最大上限！");
+		}else{	
+			amount++;
+			document.getElementById("amount").value = amount;
+		}
+	}
+	//减少购买数量
+	function numDec(){
+		var amount = parseInt(document.getElementById("amount").value);
+		if(amount <= 0){
+			return;
+		}else{	
+			amount--;
+			document.getElementById("amount").value = amount;
+		}
+	}
+	
